cd2357e2f6512759b114762669cc3bcd17b093c3
sadotsoy <sadot@ocho.co>
2023-09-11 16:32:58 -0600

    Add Telescope file browser

---
+++ after/plugin/telescope.rc.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_set_keymap(
+  "n",
+  "<space>fb",
+  ":Telescope file_browser<CR>",
+  { noremap = true }
+)
+
+-- open file_browser with the path of the current buffer
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<space>fb",
+--   ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+--   { noremap = true }
+-- )
+
